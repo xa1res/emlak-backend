a8a5957bd9913b7e40d959b004480cae7956932e
xaires <metexairesdugan@gmail.com>
2025-08-15 08:54:20 +0300

    asd

--- program.cs
+++ program.cs
@@ -2,17 +2,13 @@ using PentaNest.Emlak.Api.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Controllers
 builder.Services.AddControllers();
 
-// Swagger (opsiyonel ama yerinde dursun)
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// DataService (mock veriler buradan geliyor)
 builder.Services.AddSingleton<DataService>();
 
-// CORS: Angular lokal için izin ver
 var allowedOrigin = builder.Configuration["FRONTEND_URL"] ?? "http://localhost:4200";
 builder.Services.AddCors(o =>
 {
@@ -24,19 +20,15 @@ builder.Services.AddCors(o =>
 
 var app = builder.Build();
 
-// Swagger
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-// HTTP -> HTTPS yönlendirmeye şimdilik gerek yoksa kapat
-// app.UseHttpsRedirection();
 
 app.UseCors("frontend");
 
-// Attribute-routed controller'ları map'le
 app.MapControllers();
 
 app.Run();
